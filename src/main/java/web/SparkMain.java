b265ba1c21cd388e510eb342a57a61d486ebd317
Ovidiu Lipianu <ovidexter4@gmail.com>
2021-07-21 19:53:02 +0300

    Get

--- src/main/java/web/SparkMain.java
+++ src/main/java/web/SparkMain.java
@@ -4,7 +4,16 @@ import static spark.Spark.*;
 
 public class SparkMain {
     public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World");
+        port(8081);
+        get("/hello", (req, res) -> {
+            String param=req.queryParams("name");
+            res.status(201);
+            return "Hello World "+param;});
+        get("/hello/:name/word",(req,res)->{
+            String param=req.params(":name");
+            res.status(201);
+            return "Hello World "+param;
+        });
     }
 }
 
